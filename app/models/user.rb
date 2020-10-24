711437dee28f90b9ee9ed19a88746462b263d5d3
yutaro-kugishima <yocw6389@yahoo.co.jp>
2020-10-24 18:37:47 +0900

    テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name_kana, presence: true
   validates :birth_date, presence: true
 
 end
