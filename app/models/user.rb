3ded29a24980878d068d36a79b46af77d3525e5b
yutaro-kugishima <yocw6389@yahoo.co.jp>
2020-10-26 21:51:18 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,5 @@ class User < ApplicationRecord
     validates :last_name_kana, format: { with: /\A[ァ-ン]+\z/, message: '全角文字（カナ）を使用してください' }
     validates :birth_date
     validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '6文字以上にしてください' }
-    validates :password_confrimation,format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '6文字以上にしてください' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,43 +1,47 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
-    it "nicknameが空だと登録できない" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+    context "nicknameが空だと登録できない" do
+      it (nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
       user.valid?
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
-    it "名字が空だと登録できない" do
-      user = User.new(nickname: "abe", first_name: "", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
+    context "名字が空だと登録できない" do
+      it (nickname: "abe", first_name: "", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
       user.valid?
       expect(user.errors.full_messages).to include("First name can't be blank")
     end
-    it "名前が空だと登録できない" do
-      user = User.new(nickname: "abe", last_name: "", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
+    context "名前が空だと登録できない" do
+      it (nickname: "abe", last_name: "", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
       user.valid?
       expect(user.errors.full_messages).to include("Last name can't be blank")
     end
-    it "名字（カナ）が空だと登録できない" do
-      user = User.new(nickname: "abe", first_name_kana: "", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
+    context "名字（カナ）が空だと登録できない" do
+      it (nickname: "abe", first_name_kana: "", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
       user.valid?
       expect(user.errors.full_messages).to include("First name kana can't be blank")
     end
-    it "名前（カナ）が空だと登録できない" do
-      user = User.new(nickname: "abe", last_name_kana: "", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
+    context "名前（カナ）が空だと登録できない" do
+      it (nickname: "abe", last_name_kana: "", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
       user.valid?
       expect(user.errors.full_messages).to include("Last name kana can't be blank")
     end
-    it "誕生日が空だと登録できない" do
-      user = User.new(nickname: "abe", birth_date: "", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
+    context "誕生日が空だと登録できない" do
+      it (nickname: "abe", birth_date: "", email: "kkk@gmail.com", password: "", password_confirmation: "00000000")
       user.valid?
       expect(user.errors.full_messages).to include("Birth date can't be blank")
     end
-    it "passwordが空では登録できない" do
-      user = User.new(nickname: "abe", email: "", password: "", password_confirmation: "00000000")
+    context "passwordが空では登録できない" do
+      it (nickname: "abe", email: "", password: "", password_confirmation: "00000000")
       user.valid?
       expect(user.errors.full_messages).to include("Password can't be blank")
     end
-    it "password（確認）が空では登録できない" do
-      user = User.new(nickname: "abe", email: "", password: "00000000", password_confirmation: "")
+    context "password（確認）が空では登録できない" do
+      it (nickname: "abe", email: "", password: "00000000", password_confirmation: "")
       user.valid?
       expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
