e5adce83a998c65fd1651414336738cfb3d32a55
yutaro-kugishima <yocw6389@yahoo.co.jp>
2020-11-07 20:23:25 +0900

    商品出品機能の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,7 +2,6 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    // console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     const tax = Math.floor(inputValue / 10);
     addTaxDom.innerHTML = tax;
