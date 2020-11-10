014836e8de28b5b73fdf9e285cd3bf22a6c03c0d
yutaro-kugishima <yocw6389@yahoo.co.jp>
2020-11-10 21:15:24 +0900

    商品編集機能の実装修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
     before_action :authenticate_user!, only: [:new]
-    before_action :set_item, only: [:show, :edit]
+    before_action :set_item, only: [:show, :edit, :update]
 
 
   def index
@@ -28,9 +28,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(item_params)
-      redirect_to root_path
+    if @item.update(item_params)
+      redirect_to item_path
     else
       render :edit
     end
